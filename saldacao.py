f8abf9c0e6cf1bcb63a2248446089e14d29da452
pedro <pedro.botega@edu.senai.br>
2026-05-19 11:05:00 -0300

    Add saudacao

--- saldacao.py
+++
@@ -1,4 +0,0 @@
-def saudaçao (nome):
-  return f'boa tarde, {nome}'
-
- print (saudacao ( 'botega'))
---
+++ saudacao.py
@@ -0,0 +1,4 @@
+def saudaçao (nome):
+  return f'bom dia, {nome}'
+
+ print (saudacao ( 'botega'))
