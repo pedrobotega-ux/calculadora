f6a6a49b151bdb6e0daafb8c781307694ac7a495
pedro <pedro.botega@edu.senai.br>
2026-05-26 08:10:57 -0300

    add funcao de soma

--- saudacao.py
+++ saudacao.py
@@ -1,4 +1,8 @@
 def saudaçao (nome):
-  return f'bom dia, {nome}'
+  return f'tenha um otimo final de semana, {nome}'
+
+def soma (n1,n2):
+  resultado = n1 + n2
+  return(resultado)
 
  print (saudacao ( 'botega'))
